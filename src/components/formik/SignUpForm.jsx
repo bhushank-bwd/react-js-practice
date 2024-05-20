3425a0458bd29263bf0c84cd1c1fdb3cc1e5e6c9
bhushanK <bhushank@noitavonne.com>
2024-05-20 22:48:04 +0530

    formik and yup validation 1

--- src/components/formik/SignUpForm.jsx
+++ src/components/formik/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import { useFormik } from "formik";
+import { Field, Form, Formik, useFormik } from "formik";
 import React from "react";
 import * as Yup from "yup";
 const SignUpForm = () => {
@@ -8,91 +8,89 @@ const SignUpForm = () => {
     password: "",
     confirm_password: "",
   };
-  const { handleBlur, handleChange, handleSubmit, values, errors } = useFormik({
-    initialValues: initialValues,
-    validationSchema: signupValidation,
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+  // const { handleBlur, handleChange, handleSubmit, values, errors } = useFormik({
+  //   initialValues: initialValues,
+  //   validationSchema: signupValidation,
+  //   onSubmit: (values) => {
+  //     console.log(values);
+  //   },
+  // });
   return (
     <div className="mx-auto">
-      <form onSubmit={handleSubmit}>
-        <div className="p-2">
-          <label htmlFor="name">Name</label>
-          <br />
-          <input
-            name="name"
-            className="border border-black outline-1"
-            id="name"
-            type="text"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.name}
-          />{" "}
-          {errors.name && (
-            <small className="text-sm text-red-500">{errors.name}</small>
-          )}
-        </div>
-        <div className="p-2">
-          <label htmlFor="email">Email</label>
-          <br />
-          <input
-            name="email"
-            className="border border-black outline-1"
-            id="email"
-            type="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          {errors.email && (
-            <small className="text-sm text-red-500">{errors.email}</small>
-          )}
-        </div>
-        <div className="p-2">
-          <label htmlFor="password">Password</label>
-          <br />
-          <input
-            name="password"
-            className="border border-black outline-1"
-            id="password"
-            type="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && (
-            <small className="text-sm text-red-500">{errors.password}</small>
-          )}
-        </div>
-        <div className="p-2">
-          <label htmlFor="confirm_password">Confirm password</label>
-          <br />
-          <input
-            className="border border-black outline-1"
-            name="confirm_password"
-            id="confirm_password"
-            type="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.confirm_password}
-          />
-          {errors.confirm_password && (
-            <small className="text-sm text-red-500">
-              {errors.confirm_password}
-            </small>
-          )}
-        </div>
-        <div className="p-2">
-          <button
-            className="p-2 bg-blue-700 text-white rounded-sm w-32 text-md"
-            type="submit"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={signupValidation}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ errors }) => (
+          <Form>
+            <div className="p-2">
+              <label htmlFor="name">Name</label>
+              <br />
+              <Field
+                name="name"
+                className="border border-black outline-1"
+                id="name"
+                type="text"
+              />{" "}
+              {errors.name && (
+                <small className="text-sm text-red-500">{errors.name}</small>
+              )}
+            </div>
+            <div className="p-2">
+              <label htmlFor="email">Email</label>
+              <br />
+              <Field
+                name="email"
+                className="border border-black outline-1"
+                id="email"
+                type="email"
+              />
+              {errors.email && (
+                <small className="text-sm text-red-500">{errors.email}</small>
+              )}
+            </div>
+            <div className="p-2">
+              <label htmlFor="password">Password</label>
+              <br />
+              <Field
+                name="password"
+                className="border border-black outline-1"
+                id="password"
+                type="password"
+              />
+              {errors.password && (
+                <small className="text-sm text-red-500">
+                  {errors.password}
+                </small>
+              )}
+            </div>
+            <div className="p-2">
+              <label htmlFor="confirm_password">Confirm password</label>
+              <br />
+              <Field
+                className="border border-black outline-1"
+                name="confirm_password"
+                id="confirm_password"
+                type="password"
+              />
+              {errors.confirm_password && (
+                <small className="text-sm text-red-500">
+                  {errors.confirm_password}
+                </small>
+              )}
+            </div>
+            <div className="p-2">
+              <button
+                className="p-2 bg-blue-700 text-white rounded-sm w-32 text-md"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
