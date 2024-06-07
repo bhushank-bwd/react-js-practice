a23cc7a9a8dfbc5ce99628004bd30e53082edf93
bhushan kumbhar <bhushank-bwd@gmail.com>
2024-06-07 20:38:34 +0530

    project select and delete done

--- src/components/Udemy/project/ProjectHome.jsx
+++ src/components/Udemy/project/ProjectHome.jsx
@@ -2,17 +2,37 @@ import React, { useState } from "react";
 import ProjectSideBar from "./ProjectSideBar";
 import AddProject from "./AddProject";
 import NoProjectSelected from "./NoProjectSelected";
+import SelectedProject from "./SelectedProject";
 
 const ProjectHome = () => {
   const [projectState, setProjectState] = useState({
-    selectedProject: undefined,
+    selectedProjectId: undefined,
     projects: [],
   });
   const handleAddProject = () => {
     setProjectState((prev) => {
       return {
         ...prev,
-        selectedProject: null,
+        selectedProjectId: null,
+      };
+    });
+  };
+  const handleDeleteProject = () => {
+    setProjectState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: undefined,
+        projects: prev.projects.filter(
+          (project) => project.id !== prev.selectedProjectId
+        ),
+      };
+    });
+  };
+  const handleSelectProject = (id) => {
+    setProjectState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: id,
       };
     });
   };
@@ -25,15 +45,23 @@ const ProjectHome = () => {
     setProjectState((prev) => {
       return {
         ...prev,
-        selectedProject: undefined,
+        selectedProjectId: undefined,
         projects: [...prev.projects, tempProjectData],
       };
     });
   };
-  let content;
-  if (projectState.selectedProject === null) {
+  const selctedProject = projectState.projects.find(
+    (project) => project.id === projectState.selectedProjectId
+  );
+  let content = (
+    <SelectedProject
+      project={selctedProject}
+      deleteProject={handleDeleteProject}
+    />
+  );
+  if (projectState.selectedProjectId === null) {
     content = <AddProject addNewproject={addNewproject} />;
-  } else if (projectState.selectedProject === undefined) {
+  } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected addProject={handleAddProject} />;
   }
   return (
@@ -42,6 +70,7 @@ const ProjectHome = () => {
         <ProjectSideBar
           projectList={projectState.projects}
           addProject={handleAddProject}
+          selectProject={handleSelectProject}
         />
         {content}
       </div>
